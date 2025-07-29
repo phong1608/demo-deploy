90991fcbfd3a400d4e8d5b6b9f085be77263db8b
Hoang Phong <hoangvanphong579@gmail.com>
2025-07-29 14:36:19 +0700

    minor change

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Replace placeholder in JS/HTML with runtime env variable
+envsubst < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+
+exec "$@"
